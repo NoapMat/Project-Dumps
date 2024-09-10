77410e1195a27b29a7f5b3ac0d019e12af96ac1b
NoapMat <mathewapon@gmail.com>
2024-09-10 22:08:19 +0800

    additions: smalls (#1)
    * mode: create introduce.cpp
    
    * mode: create helloworld.cpp
    
    * mode: create tempconverter.cpp
    
    * mode: create calculator.cpp

---
+++ C++/calculator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+// @TODO: Create a simple calculator, with 4 fundamental operations
+int main() {
+	while (1) {
+		int n1, n2, operation;
+		string choice;
+		double result;
+		
+		cout << "Enter your First number: ";
+		cin >> n1;
+		
+		cout << "Enter your Second number: ";
+		cin >> n2;
+		
+		cout << "Select Your Operations:\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication\n:>>";
+		cin >> operation;
+		
+		switch (operation) {
+			case 1:
+				result = n1 + n2;
+				break;
+			case 2:
+				result = n1 - n2;
+				break;
+			case 3:
+				result = n2 / n1;
+				break;
+			case 4:
+				result = n1 * n2;
+				break;
+		}
+		cout << "Result: " << result << endl;
+		cout << "Do you want to continue?: (y/n) ";
+		cin >> choice;
+		
+		if (choice == "n" || choice == "no" || choice == "N") {
+			break;
+		}
+	}
+}
---
+++ C++/introduce.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+// @TODO: Introduce Yourself using Sequential Programming
+int main() {
+	cout << "I'm Mat" << endl;
+	cout << "A student from <redacted>";
+	return 0;
+}
