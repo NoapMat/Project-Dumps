e9e2413c8f89bc3b2b64d8def66b6dc3d3493ac6
NoapMat <mathewapon@gmail.com>
2024-09-10 23:05:02 +0900

    mode: create tempconverter.cpp

---
+++ C++/tempconverter.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+// @TODO: Temperature, Celsius to Farenheit converter
+int main() {
+	int choice, temp;
+	string sign;
+	double result;
+	cout << "Enter your temperature: ";
+	cin >> temp;
+	cout << "Pick One:\n1. Celsius to Farenheit\n2. Farenheit to Celsius\n: ";
+	cin >> choice;
+	switch (choice) {
+		case 1:
+			result = temp * 1.8 + 32;
+			sign = "°F";
+			break;
+		case 2:
+			result = (temp - 32) * 5 / 9;
+			sign = "°C";
+			break;
+	}
+	cout << "Your Temperature was: " << result << sign << endl;
+}
