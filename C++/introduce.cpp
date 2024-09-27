7b23253b77e44c85059cb1f2c2ea22ed03d0a5dc
NoapMat <mathewapon@gmail.com>
2024-09-27 13:52:25 +0800

    chore: appraise the linter (#2)
    * chore: appraise the linter
    
    * chore: appraise the linter
    
    #2 file: helloworld.cpp
    
    * chore: appraise the linter
    
    #2 file: introduce.cpp
    
    * chore: appraise the linter
    
    #2 file: tempconverter.cpp

--- C++/introduce.cpp
+++ C++/introduce.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // @TODO: Introduce Yourself using Sequential Programming
 int main() {
-	cout << "I'm Mat" << endl;
-	cout << "A student from <redacted>";
-	return 0;
+    cout << "I'm Mat" << endl;
+    cout << "A student from <redacted>";
+    return 0;
 }
--- C++/tempconverter.cpp
+++ C++/tempconverter.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 // @TODO: Temperature, Celsius to Farenheit converter
 int main() {
-	int choice, temp;
-	string sign;
-	double result;
-	cout << "Enter your temperature: ";
-	cin >> temp;
-	cout << "Pick One:\n1. Celsius to Farenheit\n2. Farenheit to Celsius\n: ";
-	cin >> choice;
-	switch (choice) {
-		case 1:
-			result = temp * 1.8 + 32;
-			sign = "°F";
-			break;
-		case 2:
-			result = (temp - 32) * 5 / 9;
-			sign = "°C";
-			break;
-	}
-	cout << "Your Temperature was: " << result << sign << endl;
+   int choice, temp;
+   string sign;
+   double result;
+   cout << "Enter your temperature: ";
+   cin >> temp;
+   cout << "Pick One:\n1. Celsius to Farenheit\n2. Farenheit to Celsius\n: ";
+   cin >> choice;
+   switch (choice) {
+      case 1:
+         result = temp * 1.8 + 32;
+         sign = "°F";
+         break;
+      case 2:
+         result = (temp - 32) * 5 / 9;
+         sign = "°C";
+         break;
+   }
+   cout << "Your Temperature was: " << result << sign << endl;
 }
