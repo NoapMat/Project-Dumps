68784d083dc664e680f66d85c7669d2149a5b6f1
NoapMat <mathewapon@gmail.com>
2024-10-03 10:02:57 +0900

    chore: google-style docstring format
    for well-documented
    follows python's idiomatic style.

--- Python/pyramid.py
+++ Python/pyramid.py
@@ -5,8 +5,13 @@ arg1: int = sys.argv[1]
 
 def pyramid(layer: int) -> str:
     """
-    pyramid(): creates pyramid.
-        layer: number of layers
+    Creates pyramid.
+    
+    Args:
+        layer (int): number of layers
+
+    Returns:
+        str: outputs ascii art pyramid
     """
     for i in range(1, int(layer)+1):
         for j in range(1, i+1):
